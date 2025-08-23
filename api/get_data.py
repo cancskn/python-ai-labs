bbfa6b7cb226ef5bf1b158a8b092b0ad072a36c4
Can <cancskn.esc@gmail.com>
2025-08-23 17:34:44 +0200

    Add api

--- api/get_data.py
+++ api/get_data.py
@@ -1,9 +1,27 @@
 import requests
 
-url = 'https://jsonplaceholder.typicode.com/users'
-response = requests.get(url)
-print(response)
-if response.status_code == 200:
-    print(response.json())
-else:
-    print('unable to receive data')
+URL = 'https://jsonplaceholder.typicode.com/users/'
+response = requests.get(URL)
+
+# print(response.status_code)
+# print(response.json())
+data = response.json()
+# print(data[0]["name"])
+# print(data[0]["email"])
+
+names = []
+for user in data:
+    # print(user['name'])
+    # print(user['company']['name'])
+    if user['name'] == 'Leanne Graham':
+        print(user['address']['city'])
+
+    if user['id'] == 4:
+        print(user['address']['zipcode'])
+
+    names.append(user['name'])
+
+print(len(names))
+
+
+
