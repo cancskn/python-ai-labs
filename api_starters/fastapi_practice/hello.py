b8befd1c554160f5d90ea1c138682fd5d423eaae
Can <cancskn.esc@gmail.com>
2025-09-24 13:10:46 +0200

    .

--- api_starters/fastapi_practice/hello.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI() # Instance of FastAPI
-
-@app.get("/")  # Decorator for GET request at root URL
-async def root(): # Define async function for root endpoint
-    return {"message": "Hello World"}
